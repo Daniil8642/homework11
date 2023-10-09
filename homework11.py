24bb4694c7c6110ff6d556ad0035a16b8e7ba878
Daniil8642 <119420219+Daniil8642@users.noreply.github.com>
2023-10-09 18:00:44 +0300

    Update homework11.py

--- homework11.py
+++ homework11.py
@@ -3,91 +3,6 @@ import itertools
 from datetime import date, datetime
 
 
-def input_error(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except KeyError:
-            return "Enter user name"
-        except (ValueError, IndexError):
-            return "Give me name and phone please"
-
-    return wrapper
-
-
-class AssistantBot:
-    def __init__(self):
-        self.contacts = {}
-
-    @input_error
-    def add_contact(self, name, phone):
-        self.contacts[name] = phone
-        return f"Added {name} with phone number {phone}"
-
-    @input_error
-    def change_contact(self, name, phone):
-        if name in self.contacts:
-            self.contacts[name] = phone
-            return f"Changed phone number for {name} to {phone}"
-        else:
-            return f"Contact {name} not found"
-
-    @input_error
-    def show_phone(self, name):
-        if name in self.contacts:
-            return f"{name}'s phone number is {self.contacts[name]}"
-        else:
-            return f"Contact {name} not found"
-
-    def show_all(self):
-        if not self.contacts:
-            return "No contacts found"
-        else:
-            result = ""
-            for name, phone in self.contacts.items():
-                result += f"{name}: {phone}\n"
-            return result
-
-    def handle_command(self, command):
-        command = command.lower()
-        if command == "hello":
-            return "How can I help you?"
-        elif command.startswith("add "):
-            parts = command.split(" ", 2)
-            if len(parts) != 3:
-                return "Invalid input format"
-            name, phone = parts[1], parts[2]
-            return self.add_contact(name, phone)
-        elif command.startswith("change "):
-            parts = command.split(" ", 2)
-            if len(parts) != 3:
-                return "Invalid input format"
-            name, phone = parts[1], parts[2]
-            return self.change_contact(name, phone)
-        elif command.startswith("phone "):
-            name = command.split(" ", 1)[1]
-            return self.show_phone(name)
-        elif command == "show all":
-            return self.show_all()
-        elif command in ("good bye", "close", "exit"):
-            return "Good bye!"
-        else:
-            return "Unknown command"
-
-    def run(self):
-        while True:
-            command = input("Enter a command: ")
-            if command == ".":
-                break
-            response = self.handle_command(command)
-            print(response)
-
-
-if __name__ == "__main__":
-    bot = AssistantBot()
-    bot.run()
-
-
 class Field:
     def __init__(self, value):
         self._value = None
@@ -166,10 +81,16 @@ class Record:
 
 
 class AddressBook(UserDict):
-    def iterator(self, chunk_size):
+    def __init__(self):
+        super().__init__()
+        self.chunk_size = 5
+
+    def iterator(self):
         keys = list(self.data.keys())
-        for i in range(0, len(keys), chunk_size):
-            yield {k: self.data[k] for k in itertools.islice(keys, i, i + chunk_size)}
+        for i in range(0, len(keys), self.chunk_size):
+            yield {
+                k: self.data[k] for k in itertools.islice(keys, i, i + self.chunk_size)
+            }
 
     def add_record(self, record):
         self.data[record.name.value] = record
@@ -182,4 +103,6 @@ class AddressBook(UserDict):
             del self.data[name]
 
 
-
+if __name__ == "__main__":
+    bot = AddressBook()
+    bot.run()
